20bad639c34a1dc396a8c43643fda8a43a61776c
WebDevManuel <donmanuel2005@web.de>
2024-02-01 21:17:42 +0100

    Add(calculate-average.js): solution

---
+++ 7kyu/calculate-average.js
@@ -0,0 +1,20 @@
+`
+Write a function which calculates the average of the numbers in a given list.
+Note: Empty arrays should return 0.
+`
+
+function findAverage(array) {
+  if (array.length < 1) {
+    return 0;
+  };
+
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  };
+
+  return sum / array.length;
+}
+
+findAverage([1, 2, 3, 4]);
